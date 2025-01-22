5bdab884602a051e5140ed55d9f88f97acb4b19e
dhiejay01 <dhiejay.magduaii@gmail.com>
2025-01-22 11:40:50 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username && password) {
+    alert("Login successful!");
+  } else {
+    alert("Please fill in all fields.");
+  }
+});
